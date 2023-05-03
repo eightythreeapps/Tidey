0d055f3759860019fc3201a81777def0c122072b
Ben Reed <ben.reed@systemc.com>
2023-05-03 10:18:06 +0100

    Add symlink to Info.plist for Pre Build Script

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -4,10 +4,6 @@
 #  Tidey
 #
 #  Created by Ben Reed on 02/05/2023.
-#  
-if [ $CI ];
-then
-    echo "CI build"
-    echo "Updating Tidal API Key from environment"
-    plutil -replace TidalDiscoveryAPISubscriptionKeyPrimary -string "$OCP_APIM_SUBSCRIPTION_KEY" $CI_XCODE_PROJECT/**/Info.plist
-fi
+#
+echo "Updating Tidal API Key from environment"
+plutil -replace TidalDiscoveryAPISubscriptionKeyPrimary -string "$OCP_APIM_SUBSCRIPTION_KEY" Info.plist
