cf71bc4d5afb1182fabbfa4b860e58920412ed23
Ben Reed <ben.reed@systemc.com>
2023-09-22 12:22:24 +0100

    Fixed broken Unit Tests

--- TideyTests/TideyDataProviderTests.swift
+++ TideyTests/TideyDataProviderTests.swift
@@ -14,15 +14,11 @@ final class TideyDataProviderTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        self.dataProvider = MockDataProvider.TideDataProvider
+        self.dataProvider = MockDataProvider.TestProvider.TideDataProviderEmpty
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        
     }
 
     func testPerformanceExample() throws {
