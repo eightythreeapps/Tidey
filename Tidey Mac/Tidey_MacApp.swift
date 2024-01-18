a38e26ef0e9a94acdebcab5a5a0ec5d810d3d9b2
Ben Reed <ben.j.reed@icloud.com>
2024-01-18 15:01:28 +0000

    Added Mac Target Added NavigationSplitView to root

---
+++ Tidey Mac/Tidey_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tidey_MacApp.swift
+//  Tidey Mac
+//
+//  Created by Ben Reed on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tidey_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
