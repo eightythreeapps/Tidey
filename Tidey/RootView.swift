20c07c6ccce4c20f59ab1e70bbcfb8262c3b00ee
Ben Reed <ben.j.reed@icloud.com>
2024-01-18 20:54:11 +0000

    Added wave view and new root view

---
+++ Tidey/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Tidey
+//
+//  Created by Ben Reed on 18/01/2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
---
+++ Tidey/WaveView.swift
@@ -0,0 +1,18 @@
+//
+//  WaveView.swift
+//  Tidey
+//
+//  Created by Ben Reed on 18/01/2024.
+//
+
+import SwiftUI
+
+struct WaveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaveView()
+}
